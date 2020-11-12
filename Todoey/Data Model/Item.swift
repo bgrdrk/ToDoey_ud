4fbd5ec6982d0397939655c36dfa74ddcdd1e39d
Remigijus <remigijus@vizualiterapija.lt>
2020-11-12 11:55:58 +0200

    Github01 Test

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Remis on 2020-11-11.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+class ItemNotUsingAnymoreLeftForMemories: Encodable, Decodable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
